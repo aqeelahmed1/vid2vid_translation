737dd784774290cab5230b7e747bcd648b38b54e
Aqeel Ahmed <aqeel.ahmad@pkgcare.com>
2024-12-26 19:50:36 +0000

    v33

--- handler.py
+++ handler.py
@@ -13,14 +13,19 @@ def handler(job):
     job_input = job['input']
     print(job_input)
     vid_base64 = job_input.get('video',None)
+
     style = job_input.get('style','Ryo Takemas')
     infile='input.mp4'
     outfile='out.mp4'
-    decode_base64_to_video(vid_base64,infile)
-    main(infile,style,outfile)
-    encoded_frames=encode_video_to_base64(outfile)
-    print('finished')
-    return encoded_frames
+
+    if vid_base64 is not None:
+        decode_base64_to_video(vid_base64,infile)
+        main(infile,style,outfile)
+        encoded_frames=encode_video_to_base64(outfile)
+        print('finished')
+        return encoded_frames
+    else:
+        return {"logs":"not processed"}
 
 
 runpod.serverless.start({"handler": handler})
