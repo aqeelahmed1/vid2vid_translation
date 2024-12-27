d6c7117cf4bd5e5c228437eafc5cbfe6288e000b
Aqeel Ahmed <aqeel.ahmad@pkgcare.com>
2024-12-27 06:22:53 +0000

    xcv

--- install.py
+++ install.py
@@ -8,7 +8,7 @@ run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
 def download_model():
     path = './deps/Rerender_A_Video/models'
     if not os.path.exists(path):
-        os.makedirs(path)
+        os.makedirs(path, exist_ok=True)
         command = f"wget -P {path} 'https://huggingface.co/PKUWilliamYang/Rerender/resolve/main/models/gmflow_sintel-0c07dcb3.pth'"
         run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
 
