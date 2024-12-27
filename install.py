80b60275543a28621cb82ce11a0e1982ae27bafd
Aqeel Ahmed <aqeel.ahmad@pkgcare.com>
2024-12-27 06:10:25 +0000

    permission error

--- install.py
+++ install.py
@@ -1,6 +1,9 @@
 import os
 from subprocess import PIPE, run
 print('installing')
+command = "chmod -R u+w ./deps"
+run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
+
 
 def download_model():
     path = './deps/Rerender_A_Video/models'
